ec5aaf269e511ce0882887a9f7dcb1ee3c408989
Oleksandr_Voinalovic <Oleksandr_Voinalovich@epam.com>
2021-10-04 13:00:38 +0300

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Square square = new Square("squear", 4);
+        Square square = new Square("square", 4);
         Circle circle = new Circle("circle",5);
         Rectangle rectangle = new Rectangle("rectangle",3,4);
         System.out.println("square name is " + square.sayMyName());
